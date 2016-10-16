5b1570d490cec00274e6f6874bcd87b8fa243dc9
佐藤 優 <sgyou08@yahoo.co.jp>
2016-10-16 23:04:20 +0900

    【nicolas】凍結前のコミット

--- asset/jsx/common.jsx
+++ asset/jsx/common.jsx
@@ -1,17 +1,127 @@
-//reactを読み込み
+//ライブラリを読み込み
 var React = require('react'),
-    ReactDom = require('react-dom');
+    ReactDom = require('react-dom'),
+    Remarkable = require('remarkable'),
+    request = require('superagent');
+
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString(), {sanitize: true});
+        return {__html: rawMarkup};
+    },
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        )
+    }
+})
 
 var CommentBox = React.createClass({
+    loadCommentsFromServer: function() {
+        request.get(this.props.url)
+            .end(function(err, res) {
+                if(res.ok) {
+                    this.setState({data: res.body})
+                } else {
+                    console.log('error');
+                }
+            }.bind(this));
+    },
+    handleCommentSubmit: function(comment) {
+        request.post(this.props.url)
+            .type('form')
+            .send({data: comment})
+            .end(function(err, res) {
+                if(res.ok) {
+                    this.setState({data: res.body})
+                } else {
+                    console.log('error');
+                }
+            }.bind(this));
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         );
     }
 });
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            )
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    getInitialState: function() {
+        return {author: '', text: ''};
+    },
+    handleAuthorChange: function(e) {
+        this.setState({author: e.target.value});
+    },
+    handleTextChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if(!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, text: text});
+        this.setState({author: '', text: ''});
+    },
+    render: function() {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
+        );
+    }
+});
+
 ReactDom.render(
-    <CommentBox />,
+    <CommentBox url="/nicolas/api/comments.json" pollInterval={2000} />,
     document.getElementById('content')
 );
